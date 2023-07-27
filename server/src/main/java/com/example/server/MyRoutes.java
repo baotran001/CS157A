10656b3f51913b2897d3d184ea18a6876701e814
Hai Ly <hai.ly@sjsu.edu>
2023-07-27 12:35:55 -0700

    Added Register UI

---
+++ server/src/main/java/com/example/server/MyRoutes.java
@@ -0,0 +1,17 @@
+package com.example.server;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/quizMeDB")
+public class MyRoutes {
+    @GetMapping("/register")
+    public String register(Model model){
+        String message = "Hello from Spring Boot!";
+        model.addAttribute("message", message);
+        return "register";
+    }
+}
