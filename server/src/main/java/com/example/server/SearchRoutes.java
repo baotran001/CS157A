f5e4604509e80774b41f70aca76a5cc141454364
Baotran <baotran.nguyen@sjsu.edu>
2023-08-06 01:16:48 -0500

    flash and user button works

--- server/src/main/java/com/example/server/SearchRoutes.java
+++ server/src/main/java/com/example/server/SearchRoutes.java
@@ -19,14 +19,12 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/quizMeDB")
-public class SearchFlashcardsRoutes {
+public class SearchRoutes {
     @GetMapping("/searchflashcards")
     public String displaySetsPage(@CookieValue(name = "user_uid", required = false) Cookie cookie, Model model) throws SQLException{
         if(cookie != null){
             model.addAttribute("cookieName",cookie.getValue());
         }
-        Connection connection = Utility.createSQLConnection();
-        Statement statement = connection.createStatement();
        
         return "searchflashcards";
     }
@@ -36,6 +34,8 @@ public class SearchFlashcardsRoutes {
         if(cookie != null){
             model.addAttribute("cookieName",cookie.getValue());
         }
+        // Check if searchKeywords is null or empty, and provide a default value if necessary
+        String searchQuery = (searchKeywords != null && !searchKeywords.isEmpty()) ? searchKeywords : "Not specified";
         Connection connection = null;
         PreparedStatement statement = null;
         ResultSet resultSet = null;
@@ -55,17 +55,16 @@ public class SearchFlashcardsRoutes {
 
             // Execute the query and get the result set
             resultSet = statement.executeQuery();
-
+             // Add the search query to the model as an attribute.
+            model.addAttribute("searched", searchQuery);
             // Process the search results and add them to the searchResults ArrayList
             while (resultSet.next()) {
-                String sid = resultSet.getString("sid");
                 String name = resultSet.getString("name");
                 String author = resultSet.getString("author");
                 Date date = resultSet.getDate("date");
                 String description = resultSet.getString("description");
 
                 Sets sets = new Sets();
-                sets.setSetid(sid);
                 sets.setName(name);
                 sets.setAuthor(author);
                 sets.setDate(date);
@@ -92,5 +91,60 @@ public class SearchFlashcardsRoutes {
         }
 
         return "searchflashcards"; // Return the same view to display the search results.
+    }
+    @PostMapping("/searchusers")
+    public String searchUser(@RequestParam("searched") String searchKeywords, Model model,
+                            @CookieValue(name = "user_uid", required = false) Cookie cookie) throws SQLException {
+        if(cookie != null){
+            model.addAttribute("cookieName",cookie.getValue());
+        }
+        // Check if searchKeywords is null or empty, and provide a default value if necessary
+        String searchQuery = (searchKeywords != null && !searchKeywords.isEmpty()) ? searchKeywords : "Not specified";
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        ArrayList<String> searchResults = new ArrayList<>();
+
+        try {
+            // Establish a connection to the database
+            connection = Utility.createSQLConnection();
+    
+            // Prepare the SQL query to search for users by name
+            String query = "SELECT * FROM Users WHERE uid= ?";
+            statement = connection.prepareStatement(query);
+    
+            // Set the searchKeywords as the parameter in the query
+            statement.setString(1, searchKeywords);
+    
+            // Execute the query and get the result set
+            resultSet = statement.executeQuery();
+    
+            // Process the search results and add user names to the searchResults ArrayList
+            while (resultSet.next()) {
+                String name = resultSet.getString("uid");
+                searchResults.add(name);
+            }
+    
+            // Add the user search results to the model as an attribute.
+            model.addAttribute("users", searchResults);
+            // Add the search query to the model as an attribute.
+            model.addAttribute("searched", searchQuery);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Close the result set, statement, and connection
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    
+
+        return "searchusers"; // Return the same view to display the search results.
     }
 }
