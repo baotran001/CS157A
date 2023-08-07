54c842a1c88c598da261ac7881759b546fbf8d1b
Isabel Luong <isabel.luong@sjsu.edu>
2023-08-06 18:52:47 -0700

    tag works, sets has tags works as well

---
+++ server/src/main/java/com/example/server/Tag.java
@@ -0,0 +1,28 @@
+package com.example.server;
+
+public class Tag {
+    private String tid;
+    private String tag_name;
+
+    // Constructor
+    public Tag() {
+        //
+    }
+
+    // Getters and setters
+    public String getTid() {
+        return tid;
+    }
+
+    public void setTid(String tid) {
+        this.tid = tid;
+    }
+
+    public String getTag_name() {
+        return tag_name;
+    }
+
+    public void setTag_name(String tag_name) {
+        this.tag_name = tag_name;
+    }
+}
